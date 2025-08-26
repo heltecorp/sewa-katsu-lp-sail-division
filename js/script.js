c3b4bb568da11efcbf1b0d6ca5b5759d7094729a
Shin H <shinh@oonoshingonoMacBook-Air.local>
2025-08-26 20:31:17 +0900

    fix(share): 共有URLを正しいリンクに修正

--- js/script.js
+++ js/script.js
@@ -33,7 +33,7 @@ document.addEventListener('DOMContentLoaded', function() {
         stepNames: ['基本情報', 'スキル・ご経験', 'ご希望'],
         gasWebAppUrl: 'https://script.google.com/macros/s/AKfycby5MHyTzB6dgLUqOJ-hd24W-7NaM59of1gRItA2rmwTRhafonJjCxcZ8dVj9fJU4qtX/exec',
         jicooRedirectUrl: 'https://www.jicoo.com/t/helte/e/l18IvvwC9O_u',
-        referralBaseUrl: 'https://sewakatsu.example.com/referral?code=ABC123XYZ',
+        referralBaseUrl: 'https://shin8899.github.io/sewa-katsu-referral/',
         referralText: `日本のホテルで働きたいあなたへ！「世話カツ」が履歴書作成から面接まで、あなたの就職を無料でフルサポートします。まずは、下のリンクから詳細をチェック！`
     };
 
